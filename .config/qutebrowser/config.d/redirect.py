a91805a6571164eacc392845f982dc85107e7734
Koxiaet <38139193+Koxiaet@users.noreply.github.com>
2020-05-16 06:52:35 +0100

    Updated st, added «», Unified documentation in docs.rs

--- .config/qutebrowser/config.d/redirect.py
+++ .config/qutebrowser/config.d/redirect.py
@@ -5,16 +5,16 @@ from PyQt5.QtCore import QUrl
 
 REDIRECT_REGEXES = [
     (
-        r"^https://doc\.rust-lang\.org(?!/stable/std)(?:/stable|/beta|/nightly|)/(?:std|alloc|core)",
+        r"^https://doc\.rust-lang\.org(?!/stable/std)(?:/stable|/beta|/nightly|)/(?:std|alloc|core(?!/arch))",
         r"https://doc.rust-lang.org/stable/std",
     ),
     (
-        r"^https://docs\.rs/(erased_serde|serde|serde_bytes|serde_derive|serde_json|serde_test|serde_transcode|serde_yaml)/(?:[^/]+)/",
-        r"https://docs.serde.rs/",
+        r"^https://docs\.serde\.rs/(erased_serde|serde|serde_bytes|serde_derive|serde_json|serde_test|serde_transcode|serde_yaml)/",
+        r"https://docs.rs/\1/*/\1/",
     ),
     (
-        r"^https://docs\.rs/unicode-(normalization|segmentation|width|xid)/(?:[^/]+)/",
-        r"https://unicode-rs.github.io/unicode-\1/",
+        r"^https://unicode-rs\.github\.io/unicode-(normalization|segmentation|width|xid)/",
+        r"https://docs.rs/unicode-\1/*/",
     ),
 ]
 
